fb3e8fb8255cd8689768dc1ebaf607c830398d0d
chandu89 <chandanwtb2009@gmail.com>
2018-10-28 16:17:00 +0530

    Infinit loop for commands and spec changes for table class

--- lib/table.rb
+++ lib/table.rb
@@ -1,5 +1,6 @@
-#  thi s class for table related works
+#  this class for table related works
 class Table
+  attr_reader :x, :y
   def initialize(x = 5, y = 5)
     @x = x
     @y = y
--- toy_robot.rb
+++ toy_robot.rb
@@ -2,3 +2,16 @@
 @table = Table.new
  # it will verify position is valid or not
 puts @table.valid_position?(1, 0)
+
+ # infinit loop for taking inputs
+input = ""
+while "infinite"
+  puts 'Enter your command'
+  input = gets.chomp
+  unless input == 'EXIT'
+    puts 'Robot has been placed' if input == 'PLACE'
+    next
+  end
+  puts 'Goodbye!'
+  break
+end
